7e59c85b2d93c975e7ad00f3fb6e6b53c5889803
efilenko74 <101466589+efilenko74@users.noreply.github.com>
2023-01-26 20:39:40 +0200

    Add files via upload

---
+++ apis/zhoraos/updater.py
@@ -0,0 +1,53 @@
+import requests
+import urllib.request
+
+a=requests.get('https://efilenko74.github.io/zhoranet/apis/zhoraos/version.txt').text
+
+if int(a)==1:
+	print('Установлена актуальная версия, обновление не требуется')
+	print('для выхода нажмите Enter...')
+	input()
+else:
+	print('Доступна новая версия, если хотите установить введите "y", если нет "n"')
+	var=input(': ')
+	if (var)=='y':
+		print('Доступна новая версия, выполняется скачивание, подождите...')
+		destination = 'system86.pyw'
+		url = 'https://efilenko74.github.io/zhoranet/apis/zhoraos/system86.pyw'
+		urllib.request.urlretrieve(url, destination)
+		print('Dowloaded 1/9')
+		destination = 'ZhoraOS.pyw'
+		url = 'https://efilenko74.github.io/zhoranet/apis/zhoraos/ZhoraOS.pyw'
+		urllib.request.urlretrieve(url, destination)
+		print('Dowloaded 2/9')
+		destination = 'setapp1.pyw'
+		url = 'https://efilenko74.github.io/zhoranet/apis/zhoraos/setapp1.pyw'
+		urllib.request.urlretrieve(url, destination)
+		print('Dowloaded 3/9')
+		destination = 'setapp2.pyw'
+		url = 'https://efilenko74.github.io/zhoranet/apis/zhoraos/setapp2.pyw'
+		urllib.request.urlretrieve(url, destination)
+		print('Dowloaded 4/9')
+		destination = 'setapp3.pyw'
+		url = 'https://efilenko74.github.io/zhoranet/apis/zhoraos/setapp3.pyw'
+		urllib.request.urlretrieve(url, destination)
+		print('Dowloaded 5/9')
+		destination = 'setpass.pyw'
+		url = 'https://efilenko74.github.io/zhoranet/apis/zhoraos/setpass.pyw'
+		urllib.request.urlretrieve(url, destination)
+		print('Dowloaded 6/9')
+		destination = 'setupweb.pyw'
+		url = 'https://efilenko74.github.io/zhoranet/apis/zhoraos/setupweb.pyw'
+		urllib.request.urlretrieve(url, destination)
+		print('Dowloaded 7/9')
+		destination = 'terminalemulator.py'
+		url = 'https://efilenko74.github.io/zhoranet/apis/zhoraos/terminalemulator.py'
+		urllib.request.urlretrieve(url, destination)
+		print('Dowloaded 8/9')
+		destination = 'updater.py'
+		url = 'https://efilenko74.github.io/zhoranet/apis/zhoraos/updater.py'
+		urllib.request.urlretrieve(url, destination)
+		print('Файлы загружены, обновление успешно, для выхода нажмите Enter...')
+		input()
+	else:
+		print('exit')
